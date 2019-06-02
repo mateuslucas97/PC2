9fb2bed1b3ba33ab41f1461a6177d9ad50813e64
mateuslucas97 <31663843+mateuslucas97@users.noreply.github.com>
2019-06-01 22:48:25 -0300

    Add files via upload

---
+++ Jogo_da_Velha/src/jogo_da_velha/Teste.java
@@ -0,0 +1,15 @@
+
+package jogo_da_velha;
+
+/**
+ *
+ * @author mateu
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        VelhaFrame vf = new VelhaFrame();
+        vf.setVisible(true);
+    }
+    
+}
