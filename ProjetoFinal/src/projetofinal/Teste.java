725fdaf551fb081fd993c9b6b1a1656c6a94c1f9
mateuslucas97 <31663843+mateuslucas97@users.noreply.github.com>
2019-08-08 20:13:44 -0300

    Add files via upload

---
+++ ProjetoFinal/src/projetofinal/Teste.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofinal;
+
+/**
+ *
+ * @author aluno
+ */
+public class Teste {
+    public static void main(String[] args) {
+        JPrincipal janela = new JPrincipal();
+    janela.setSize(500,550);
+    janela.setVisible(true);
+    }
+    
+}
